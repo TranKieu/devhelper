ce00f60fa1d568ff0f35b7fceb7c8f1403aa7aa2
trankieu.io <tranvd2010@gmail.com>
2022-11-23 21:29:10 +0100

    tailwind

--- src/commands/tailwind.command.ts
+++ src/commands/tailwind.command.ts
@@ -1,23 +1,44 @@
 import chalk from 'chalk';
 import path from 'path';
-import { copyDir, isExists } from '../utils/file.utils';
+import { copyDir, isExists, writeFile } from '../utils/file.utils';
 import { createPackage } from '../utils/create.package';
 import { createGitig } from './gitig.command';
 import { createPrettier } from './prettier.command';
+
+const tailwindArr: Array<string> = [
+  '@tailwind base;',
+  '@tailwind components;',
+  '@tailwind utilities;',
+  '@layer base {  html {   height: 100%;  }}'
+];
+
+const tailwindConfig = {
+  content: ['./public/**/*.{html,js}'],
+
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
 
 export const tailwind = async (projectName: string) => {
   /** package */
   const main = 'public/index.html';
   const scripts = {
-    build: 'postcss ./src/tailwind.css -o ./public/css/styles.css --verbose',
-    prod: 'postcss ./src/tailwind.css -o ./public/css/styles.css --env production --verbose'
+    watch: 'tailwindcss -i ./tailwind.css -o ./public/style.css --watch'
   };
+  // build: 'postcss ./src/tailwind.css -o ./public/css/styles.css --verbose',
+  // prod: 'postcss ./src/tailwind.css -o ./public/css/styles.css --env production --verbose'
 
   const devDependencies: string[] = [
-    'autoprefixer',
-    'tailwindcss',
-    'postcss-cli',
-    'cssnano'
+    // 'autoprefixer',
+    'tailwindcss'
+    // 'postcss-cli',
+    // 'cssnano'
   ];
 
   /** Create Dir */
@@ -36,7 +57,17 @@ export const tailwind = async (projectName: string) => {
   const prjTpl = path.resolve(__dirname, '../templates/tailwind');
   await copyDir(prjTpl, projectName);
 
-  // package
+  // tailwind.config.js
+  const configInhalt =
+    'module.exports =' + JSON.stringify(tailwindConfig, undefined, 2);
+  const configFile = path.resolve(projectName, 'tailwind.config.js');
+  await writeFile(configFile, configInhalt);
+  // tailwind.css await writeFile
+  const cssFile = path.resolve(projectName, 'tailwind.css');
+
+  const cssInhalt = tailwindArr.join('\n');
+  await writeFile(cssFile, cssInhalt);
+  // packageprojectName
   await createPackage(projectName, main, scripts, devDependencies);
   await createPrettier(projectName);
   await createGitig(projectName);
